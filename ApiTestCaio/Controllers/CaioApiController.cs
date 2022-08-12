85d3ff728fcc9e67fa13a1cc569dd56378da5a7b
caio2296 <52756306+caio2296@users.noreply.github.com>
2022-08-12 08:16:13 -0300

    ConexaoSql
    Isolei os metodos Sql

--- ApiTestCaio/Controllers/CaioApiController.cs
+++ ApiTestCaio/Controllers/CaioApiController.cs
@@ -21,117 +21,20 @@ namespace ApiTestCaio.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return SqlGets();
+            return ConexaoSql.SqlGets();
 
         }
 
-        private static IEnumerable<string> SqlGets()
-        {
-            
-            using (var conexao = new SqlConnection())
-            {
-               
-                conexao.ConnectionString =
-@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-                using (SqlCommand comando =
-                    new SqlCommand(@"select nome , idade from pessoa", conexao))
-                {
-
-                    List<string> Pessoa = new List<string>();
-                    conexao.Open();
-                    var leitor = comando.ExecuteReader();
-                    while (leitor.Read())
-                    {
-
-                        Pessoa.Add(leitor[0].ToString());
-
-                        Pessoa.Add(leitor[1].ToString());
-                        
-                        
-                    }
 
 
-                    return Pessoa;
-
-
-                }
-
-            }
-        }
-
-
-        private static int SlqGetsId()
-        {
-            int ids=0;
-            using (var conexao = new SqlConnection())
-            {
-
-                conexao.ConnectionString =
-@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-                using (SqlCommand comando =
-                    new SqlCommand(@"select * from pessoa", conexao))
-                {
-
-                   
-                    conexao.Open();
-                    var leitor = comando.ExecuteReader();
-                    while (leitor.Read())
-                    {
-
-                        ids = int.Parse(leitor[0].ToString());
-                    }
-                    
-                    return ids;
-                    
-
-
-                }
-
-            }
-
-
-
-        }
 
         [HttpGet("{id_pessoa}")]
         public ActionResult<string> Get(int id_pessoa)
         {
-            return SqlGet(id_pessoa);
+            return ConexaoSql.SqlGet(id_pessoa);
 
         }
 
-        private static ActionResult<string> SqlGet(int id_pessoa)
-        {
-            using (var conexao = new SqlConnection())
-            {
-                conexao.ConnectionString =
-@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-                using (SqlCommand comando =
-                    new SqlCommand(@$"select nome, idade from pessoa where id_pessoa ={id_pessoa}", conexao))
-                {
-
-                    string Pessoa = "";
-                    conexao.Open();
-                    var leitor = comando.ExecuteReader();
-                    while (leitor.Read())
-                    {
-
-                        Pessoa = (leitor[0].ToString()) + " " + (leitor[1].ToString());
-
-
-                    }
-
-
-                    return Pessoa;
-
-
-                }
-
-            }
-        }
 
 
 
@@ -164,7 +67,7 @@ namespace ApiTestCaio.Controllers
 
                 Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(pessoa);
 
-                SqlPost(myDeserializedClass);
+                ConexaoSql.SqlPost(myDeserializedClass);
 
                 
 
@@ -193,43 +96,6 @@ namespace ApiTestCaio.Controllers
 
         }
 
-        private static void SqlPost(Root myDeserializedClass)
-        {
-
-            int ids = SlqGetsId();
-
-            IEnumerable<string> enumerable = SqlGets();
-
-            using (var conexao = new SqlConnection())
-            {
-                conexao.ConnectionString =
-@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-                if(myDeserializedClass == null)
-                {
-                    throw new NullReferenceException("Referencia nula");
-
-                }
-
-
-                using (SqlCommand comando =
-                    new SqlCommand(
-                        @$"INSERT INTO pessoa (id_pessoa,nome,idade) values ({ids+1},'{myDeserializedClass.pessoa.nome}', {myDeserializedClass.pessoa.idade})", conexao))
-                {
-
-
-                    conexao.Open();
-                    comando.ExecuteNonQuery();
-
-
-
-
-
-
-                }
-
-            }
-        }
 
 
         //Put
@@ -245,38 +111,11 @@ namespace ApiTestCaio.Controllers
 
             Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(pessoa);
 
-            SqlPut(id_pessoa, myDeserializedClass);
+            ConexaoSql.SqlPut(id_pessoa, myDeserializedClass);
 
         }
 
-        private static void SqlPut(int id_pessoa, Root myDeserializedClass)
-        {
-            using (var conexao = new SqlConnection())
-            {
-                conexao.ConnectionString =
-@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-
-
-
-                using (SqlCommand comando =
-                    new SqlCommand(
-                        @$"UPDATE pessoa SET nome = '{myDeserializedClass.pessoa.nome}', idade = {myDeserializedClass.pessoa.idade} WHERE id_pessoa = {id_pessoa} ", conexao))
-                {
-
-
-                    conexao.Open();
-                    comando.ExecuteNonQuery();
-
-
-
-
-
-
-                }
-
-            }
-        }
+       
 
 
         //Delete
@@ -286,32 +125,15 @@ namespace ApiTestCaio.Controllers
         {
             //DELETE FROM pessoa WHERE id_pessoa=4;
 
-            SqlDelete(id_pessoa);
+           ConexaoSql.SqlDelete(id_pessoa);
 
         }
 
-        private static void SqlDelete(int id_pessoa)
-        {
-            using (var conexao = new SqlConnection())
-            {
-                conexao.ConnectionString =
-@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-
-                using (SqlCommand comando =
-                    new SqlCommand(@$"DELETE FROM pessoa WHERE id_pessoa={id_pessoa}", conexao))
-                {
-
-                    string Pessoa = "";
-                    conexao.Open();
-                    comando.ExecuteNonQuery();
-
-
-                }
-
-            }
-        }
     }
+
+
+
+
 }
 
 
---
+++ ApiTestCaio/Controllers/ConexaoSql.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTestCaio.Controllers
+{
+    public class ConexaoSql
+    {
+
+        //GETS: Pega todos os valores menos o Id
+        public static IEnumerable<string> SqlGets()
+        {
+
+            using (var conexao = new SqlConnection())
+            {
+
+                conexao.ConnectionString =
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                using (SqlCommand comando =
+                    new SqlCommand(@"select nome , idade from pessoa", conexao))
+                {
+
+                    List<string> Pessoa = new List<string>();
+                    conexao.Open();
+                    var leitor = comando.ExecuteReader();
+                    while (leitor.Read())
+                    {
+
+                        Pessoa.Add(leitor[0].ToString());
+
+                        Pessoa.Add(leitor[1].ToString());
+
+
+                    }
+
+
+                    return Pessoa;
+
+
+                }
+
+            }
+        }
+
+
+        //GET: PEGA O ULTIMO Id para o auto incremento do Post
+        public static int SlqGetsId()
+        {
+            int ids = 0;
+            using (var conexao = new SqlConnection())
+            {
+
+                conexao.ConnectionString =
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                using (SqlCommand comando =
+                    new SqlCommand(@"select * from pessoa", conexao))
+                {
+
+
+                    conexao.Open();
+                    var leitor = comando.ExecuteReader();
+                    while (leitor.Read())
+                    {
+
+                        ids = int.Parse(leitor[0].ToString());
+                    }
+
+                    return ids;
+
+
+
+                }
+
+            }
+
+
+
+        }
+
+        //Get: Pega um registro
+        public static ActionResult<string> SqlGet(int id_pessoa)
+        {
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString =
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                using (SqlCommand comando =
+                    new SqlCommand(@$"select nome, idade from pessoa where id_pessoa ={id_pessoa}", conexao))
+                {
+
+                    string Pessoa = "";
+                    conexao.Open();
+                    var leitor = comando.ExecuteReader();
+                    while (leitor.Read())
+                    {
+
+                        Pessoa = (leitor[0].ToString()) + " " + (leitor[1].ToString());
+
+
+                    }
+
+
+                    return Pessoa;
+
+
+                }
+
+            }
+        }
+
+
+        //Post: Fazer o registro
+        public static void SqlPost(Root myDeserializedClass)
+        {
+
+            int ids = SlqGetsId();
+
+            IEnumerable<string> enumerable = SqlGets();
+
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString =
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                if (myDeserializedClass == null)
+                {
+                    throw new NullReferenceException("Referencia nula");
+
+                }
+
+
+                using (SqlCommand comando =
+                    new SqlCommand(
+                        @$"INSERT INTO pessoa (id_pessoa,nome,idade) values ({ids + 1},'{myDeserializedClass.pessoa.nome}', {myDeserializedClass.pessoa.idade})", conexao))
+                {
+
+
+                    conexao.Open();
+                    comando.ExecuteNonQuery();
+
+
+
+
+
+
+                }
+
+            }
+        }
+
+        //Put: faz a atualização
+        public static void SqlPut(int id_pessoa, Root myDeserializedClass)
+        {
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString =
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+
+
+                using (SqlCommand comando =
+                    new SqlCommand(
+                        @$"UPDATE pessoa SET nome = '{myDeserializedClass.pessoa.nome}', idade = {myDeserializedClass.pessoa.idade} WHERE id_pessoa = {id_pessoa} ", conexao))
+                {
+
+
+                    conexao.Open();
+                    comando.ExecuteNonQuery();
+
+
+
+
+
+
+                }
+
+            }
+        }
+
+
+        //Delete: deleta o registro
+        public static void SqlDelete(int id_pessoa)
+        {
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString =
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Pessoa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+                using (SqlCommand comando =
+                    new SqlCommand(@$"DELETE FROM pessoa WHERE id_pessoa={id_pessoa}", conexao))
+                {
+
+                    string Pessoa = "";
+                    conexao.Open();
+                    comando.ExecuteNonQuery();
+
+
+                }
+
+            }
+        }
+
+
+
+    }
+}
